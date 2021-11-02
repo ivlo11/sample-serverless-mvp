eb052461003943b31885049abaf258e075c0e52e
Ivonne <ivlo11@devwidgets.io>
2021-11-02 08:40:46 -0400

    refactor from hello world to events ms

--- events-ms/src/test/java/io/devwidgets/events/api/lambda/LambdaHandlerTest.java
+++ events-ms/src/test/java/io/devwidgets/events/api/lambda/LambdaHandlerTest.java
@@ -1,16 +1,17 @@
-package helloworld;
+package io.devwidgets.events.api.lambda;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
-public class AppTest {
+public class LambdaHandlerTest {
   @Test
   public void successfulResponse() {
-    App app = new App();
-    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+    LambdaHandler lambdaHandler = new LambdaHandler();
+    APIGatewayProxyResponseEvent result = lambdaHandler.handleRequest(null, null);
     assertEquals(result.getStatusCode().intValue(), 200);
     assertEquals(result.getHeaders().get("Content-Type"), "application/json");
     String content = result.getBody();
