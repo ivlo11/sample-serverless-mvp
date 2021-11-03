ec8e0bf4506ab07158d8f405b05b4a713a414c7f
Ivonne <ivlo11@devwidgets.io>
2021-11-03 09:09:53 -0400

    create custom subsegment

--- events-ms/src/main/java/io/devwidgets/events/api/lambda/LambdaHandler.java
+++ events-ms/src/main/java/io/devwidgets/events/api/lambda/LambdaHandler.java
@@ -6,14 +6,19 @@ import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.entities.Subsegment;
 import io.devwidgets.events.framework.SpringBootApplicationConfig;
 import io.devwidgets.events.framework.interceptor.LoggingInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class LambdaHandler implements RequestStreamHandler {
+  private static final Logger logger = LoggerFactory.getLogger(LambdaHandler.class);
   private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
   static {
@@ -28,6 +33,18 @@ public class LambdaHandler implements RequestStreamHandler {
   @Override
   public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
     LoggingInterceptor.init(context);
+
+    Subsegment sub = null;
+    try {
+      sub = AWSXRay.beginSubsegment("event-ms");
+    } catch (Throwable t) {
+      logger.warn("failed to create subsegment", t);
+    }
+
     handler.proxyStream(input, output, context);
+
+    if (sub != null) {
+      AWSXRay.endSubsegment();
+    }
   }
 }
